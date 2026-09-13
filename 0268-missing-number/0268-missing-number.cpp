a9fb2ca4c9a2d62b6842cee21fbdbaa4a4e230d0
Pragun Bhatt <pragununofficial@gmail.com>
2026-09-13 14:49:27 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.91%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+
+        int targetSum = (n*(n+1))/2;
+        int arrSum = 0;
+        for(int i=0;i<n;i++){
+            arrSum += nums[i];
+        }
+
+        return targetSum-arrSum;
+    }
+};
