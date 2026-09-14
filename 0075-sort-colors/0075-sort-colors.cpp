91743419dd496f4b4d754a8ea0feda3788d70f64
Pragun Bhatt <pragununofficial@gmail.com>
2026-09-14 14:46:49 +0530

    Time: 0 ms (100%), Space: 11.7 MB (49.07%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+
+        int n = nums.size();
+
+        for(int i=1;i<n;i++){
+
+            bool isSwap = false;
+
+            for(int j=0;j<(n-i);j++){
+                if(nums[j]>nums[j+1]){
+                    int temp = nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1] = temp;
+
+                    isSwap = true ;
+
+                }
+            }
+
+            if(!isSwap){
+                break;
+            }
+        }
+    }
+};
