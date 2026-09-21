917fd21144e80c639529edb70b2a799905d740b5
Pragun Bhatt <pragununofficial@gmail.com>
2026-09-21 15:45:53 +0530

    Time: 0 ms (100%), Space: 7.7 MB (46.54%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
-        char alp = coordinates[0];
-        int num = int(coordinates[1]);
 
-        int pos = alp-'a';
-        if(pos%2==0 && num%2 != 0){
+
+        if(int(coordinates[0]-'a')%2==0 && (int(coordinates[1]))%2 != 0){
             return false;
-        }else if(pos%2!=0 && num%2 == 0){
+        }else if(int(coordinates[0]-'a')%2!=0 && (int(coordinates[1]))%2 == 0){
             return false;
         }
 
